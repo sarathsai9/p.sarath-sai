b9f1cadf2bf5a7429149601e5addd2bf7911db27
sarathsai9 <162982730+sarathsai9@users.noreply.github.com>
2024-03-11 16:18:47 +0530

    Add files via upload

---
+++ file 3.py
@@ -0,0 +1,53 @@
+Bargained characters
+
+Controls and data structeres -> abc language
+Strings and its formats -> from c
+Regular expressions -> pearl
+Exception handling -> mudule 3
+Magic methods from -> forton
+
+
+flavours of 
+
+> cpython-written in c, most common implementation of python.
+> jython-java implementation of python. 
+> ironpython-implementation of the python programming language which is tightly integrated   
+
+
+
+
+
+
+
+
+
+
+
+
+print("hello")
+
+print("hello world")
+,,,
+print("hello world")
+print("hello world")
+print("hello world")
+,,,
+
+----> commenting
+1.)commenting is used for making documentation in a python file
+2.)when the line is commented , thst line will not be considered as a part of the code
+3.)when the program is executed,commented line will not be in part of execution
+
+---->Note:
+single line commenting can be done using # at the begining of the line
+     Eg:
+         #print("hello 1")
+multiple line commenting can be done
+
+ 
+import this
+print(this)
+
+# import is a keyword which is used to communicate between the python the python files.
+
+import antigravity
