1986bfa63d10dfece13490ba1e1b6c38612aa7be
sarathsai9 <162982730+sarathsai9@users.noreply.github.com>
2024-03-11 16:18:19 +0530

    Add files via upload

---
+++ file 2.py
@@ -0,0 +1,27 @@
+print("hello")
+
+print("hello world")
+,,,
+print("hello world")
+print("hello world")
+print("hello world")
+,,,
+
+----> commenting
+1.)commenting is used for making documentation in a python file
+2.)when the line is commented , thst line will not be considered as a part of the code
+3.)when the program is executed,commented line will not be in part of execution
+
+---->Note:
+single line commenting can be done using # at the begining of the line
+     Eg:
+         #print("hello 1")
+multiple line commenting can be done
+
+ 
+import this
+print(this)
+
+# import is a keyword which is used to communicate between the python the python files.
+
+import antigravity
