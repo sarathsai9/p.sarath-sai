b208669d8e0e347597ddcd82e35306db86438aeb
sarathsai9 <162982730+sarathsai9@users.noreply.github.com>
2024-03-11 16:19:03 +0530

    Add files via upload

---
+++ file 4.py
@@ -0,0 +1,16 @@
+# ---> prunt(object,sep,end,flush)
+#print has 5 parameters
+
+# object -->teh value to be printed is object
+# sep--. seperator
+# Eg
+# a = 8
+# b = 9
+# c = 89
+# print(a, b, c, sep='$')
+
+# end --> which is used to add the string at the end of the value
+# it will not keep the command to stay in small line
+# Eg:
+# print (str(a), b, end='%%')
+# print(str(c))
