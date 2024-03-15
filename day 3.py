4c3a422ebb9733b9b2c59e83abf89422f1fd4609
sarathsai9 <162982730+sarathsai9@users.noreply.github.com>
2024-03-15 16:01:31 +0530

    Add files via upload

---
+++ day 3.py
@@ -0,0 +1,160 @@
+# ! Eg:3
+# ? take values of length and breadth of a
+# ? from user and check if it is square or not.
+
+# length = int(input())
+# breadth = int(input())
+# if length==breadth:
+# print("it is a square")
+# else:
+# print("its not square")
+
+# ! Eg:4
+# ? accept the age of 2 people and display the oldest one.
+# person 1 = int(input("enter the age":))
+# person 2 = int(input("enter the age":))
+# if age
+
+
+# ! Eg:4
+# python program to check whether the
+# given integer is a multiple of both 5 and 7
+
+# n = int(input("enter the number:"))
+# if n%5==0 and n%7==0:
+# print("yes")
+# else:
+# print("no")
+
+# ! Eg:5
+# write a program to accept the cost price of a boke and display the
+# road tax to be paid
+# according to the following criteria :
+
+#           cost price (in rs)             Tax
+#          > 100000                         15 % + discount 6%
+#          < 100000                         5%
+                                  
+
+# price = int(input("enter the value:"))
+# amount =0
+# if price>=100000:
+# discount = price*(6/100)
+# value = price-discount
+# tax = value*(15/100)
+# total=(value+tax)
+# else:
+# tax = price*(5/100)
+# total = price+tax
+# print("the onroad cost of bike is: ",total)
+
+# !-------> if elif else
+# Eg:1
+# a = 7
+# b = 9
+# c = 30
+
+# if a>b and a>c:
+# print("A is greater")
+# elif b>a and b>c:
+# print("B is greater")
+# elif c>a and c>b:
+# print("C is greater")
+
+# A school has following rules for grading syste:
+# a. below 25 - F
+# b. 25 to 45 - E
+# c. 45 to 50 - D
+# d. 50 to 60 - C
+# e. 60 to 80 - B
+# f. above 80 - A
+# ask user to enter marks and print the corresponding grade.
+
+# mark = int(input("enter mark: "))
+# if mark>=80 and mark<=100:
+# print("A")
+# elif mark>=60 and mark<80:
+# print("B")
+# elif mark>=50 and mark<50:
+# print("C")
+# elif mark>=40 and mark<50:
+# print("D")
+# else:
+# print("fail")
+
+
+# Eg:6
+# ! --> short hand if else
+# Eg:1
+# a = 9
+# b = 60
+# if a>b:
+# print("A")
+# else:
+# print("B")
+
+#? --> using short hand if else
+# * rules
+# 1.) statement inside the if cindition have to be present at first
+# 2.) elif connot be used in short hand if else
+# 3.) always it have to end with else
+
+# print("A") if a>b else print("B")
+
+# ! code to check the given char is vowel or consonent
+char = input("enter the char:")
+# if char=="a" or char =='e' or char =='1' or char =='0' or char =='u':
+# print("is a vowel")
+# else:
+# print("its consonent")
+
+# ? or
+
+# str1 = "aeiouAEIOU"
+# if char in str1:
+# print("vowel")
+# else:
+# print("consonent")
+
+# ? using shorthand if else
+# char = input("enter the char: ")
+# str1 = "aeiouAEIOU"
+# print("vowels") if char in str1 else print("consonent")
+
+
+# ! ---> elife ladder using short hand if else
+# Eg:1
+# ? fibe the greatest among 3 variable using short hand if else
+# a = 8
+# b = 5
+# c = 9
+
+# print("A is grater") if a>b and a>c else print("B is greater")if b>a and b>c else print("C is greater")
+
+# ! ------------> looping   
+
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
