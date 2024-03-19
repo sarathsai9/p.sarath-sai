9c4a6863ae0bbbe1778edd549c1e6ef84bb793ca
sarathsai9 <162982730+sarathsai9@users.noreply.github.com>
2024-03-19 09:19:43 +0530

    Add files via upload

---
+++ day 7.py
@@ -0,0 +1,203 @@
+# day 7
+
+# ----> Assesment
+# 1.) d1 = {'Ten': 10, 'Twenty': 20, 'Thirty': 30}
+# d2 = {'Thirty': 30, 'Fourty': 40, 'Fifty': 50}
+# Merge two python dictionary
+# o/p --> {'Ten': 10, 'Twenty': 20, 'Thirty': 30, 'Fourty': 40, 'Fifty': 50}
+
+
+#d1 = {'Ten': 10, 'Twenty': 20, 'Thirty': 30}
+#d2 = {'Thirty': 30, 'Fourty': 40, 'Fifty': 50}
+#d1.update(d2)
+#print(d1)
+
+
+
+#2.)Python Program to determine if
+# the given Sets Are Disjoint
+# or Not without Using Inbuilt-in Functions
+
+
+#set1 = {'Python', 'Java', 'Data Science'}
+#set2 = {'ML', 'AI', 'R Language', 'Python'}
+#set3 = {'Data Analytics', 'Robotics', 'Deep L' }
+#c = 0
+#flag = 0
+#for val in range(3):
+#    c=c+1
+#    if c==1:
+#        for val in set1:
+#            if val in Set2 or val in set3:
+#               flag=1
+#               break
+#    if c==2:
+#        for val in set2:
+#            if val in set1 or val in set3:
+#                flag=1
+#                break
+#    if c==3:
+#        for val in set3:
+#            if val in set2 or val in set1:
+#                flag=1
+#                break
+#if flag==0:
+#    print("disjoint")
+#else:
+#    print("joint")
+
+#3.)list1 = ["M", "na", "i", "she"]
+# list2 = ["y", "me", "s", "ruu"]
+
+# O/P --> ['My', 'name', 'is', 'sheruu']
+
+#list1 = ["M", "na", "i", "she"]
+#list2 = ["y", "me", "s", "ruu"]
+#l3 = []
+#for val in range(len(list1)):
+#    ans = list1[val]+list2[val]
+#    l3.append(ans)
+#print(l3)
+
+# or 
+
+#i=0
+#while i<len(list1):
+#    l3.append(list[i]+list[i]) 
+#    i+=1
+#print(l3)
+    
+#! Funtions
+#? DEF
+# Function is a block of code which is used to perform a specific functionality
+# Function can be created using def keyboard
+
+#? Functions has 3 parts
+# Function declaration
+# Function definition
+# Function call
+
+# Eg:1
+def greet():
+ print("i like you!..")
+
+greet()
+greet()
+greet()
+greet()
+greet()
+greet()
+greet()
+greet()#fucntion alling  
+
+# Eg: 2
+#? function with parameter
+
+#def greeting(name):
+#    print("welcom",name)
+
+#for val range(1):
+#    username=input("enter the name:")
+#    greeting(username)
+
+# 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                
+                
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+             
+            
